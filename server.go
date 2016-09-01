4c364b5a220a6ab638bc17726f6d895b9abe280f
Brady Sullivan <brady@bsull.com>
2016-08-31 23:34:16 -0700

    Add a temporary favicon

--- server.go
+++ server.go
@@ -11,7 +11,8 @@ const version = "0.0.1"
 func main() {
 	fmt.Println("JAMA Server version ", version)
 	fmt.Println("[*] Starting server...")
-	http.HandleFunc("/", HomeHandler) // Return about page
+	http.HandleFunc("/", HomeHandler)            // Return about page
+	http.HandleFunc("/favicon.ico", iconHandler) // Return about page
 
 	http.HandleFunc("/api/user", apiHandler)    // Return about page
 	http.HandleFunc("/api/message", apiHandler) // Return about page
@@ -23,6 +24,12 @@ func main() {
 func HomeHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("[+] Serving index.html")
 	filename := "index.html"
+	http.ServeFile(w, r, filename)
+}
+
+func iconHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("[+] Serving favicon.ico")
+	filename := "favicon.ico"
 	http.ServeFile(w, r, filename)
 }
 
