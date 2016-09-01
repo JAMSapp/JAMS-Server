085d734e234f7aa0900d5af082389d743d37b54c
Brady Sullivan <brady@bsull.com>
2016-08-31 23:25:15 -0700

    Add an api doc page.

--- server.go
+++ server.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
 )
@@ -8,12 +9,25 @@ import (
 const version = "0.0.1"
 
 func main() {
+	fmt.Println("JAMA Server version ", version)
+	fmt.Println("[*] Starting server...")
 	http.HandleFunc("/", HomeHandler) // Return about page
+
+	http.HandleFunc("/api/user", apiHandler)    // Return about page
+	http.HandleFunc("/api/message", apiHandler) // Return about page
+	http.HandleFunc("/api/auth", apiHandler)    // Return about page
 
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
 
 func HomeHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("[+] Serving index.html")
 	filename := "index.html"
+	http.ServeFile(w, r, filename)
+}
+
+func apiHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("[+] Serving api.html")
+	filename := "api.html"
 	http.ServeFile(w, r, filename)
 }
