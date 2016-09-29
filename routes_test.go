287374725202253f31c2c414080e09fc7ced6e4e
Brady Sullivan <brady@bsull.com>
2016-09-28 22:09:22 -0700

    Fix failing user get test. (#5)

--- routes_test.go
+++ routes_test.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"github.com/twinj/uuid"
+	"io/ioutil"
 	"net/http"
 	"strings"
 	"testing"
@@ -36,7 +38,7 @@ func testMessage(t *testing.T) {
 	// POST /api/message
 	// Creates a new message with the given body parameter.
 	// Should return 201 on successful creation or 5** for server error.
-	r = Post("/api/message", "{\"body\": \"This is the body of the first message.\"}", t)
+	_, r = Post("/api/message", "{\"body\": \"This is the body of the first message.\"}", t)
 	if r != 201 {
 		t.Errorf("POST /api/message did not return 201, instead returned %d\n", r)
 	}
@@ -67,26 +69,30 @@ func testUser(t *testing.T) {
 		t.Errorf("GET /api/user/%s did not return 200, instead returned %d\n", id.String(), r)
 	}
 
-	id2 := uuid.NewV1()
 	// POST /api/user
 	// Create a new user
-	// TODO: Remove id from this and generate a unique one automatically.
 	// Should return 201 for created or 409 for conflict.
-	r = Post("/api/user", "{\"username\":\"asdf\", \"password\": \"fdsa\"}", t)
+	res, r := Post("/api/user", "{\"username\":\"asdf\", \"password\": \"fdsa\"}", t)
 	if r != 201 && r != 409 {
 		t.Errorf("POST /api/user did not return 201, instead returned %d\n", r)
 	}
 
+	var user User
+	err := json.Unmarshal(res, &user)
+	if err != nil {
+		t.Errorf(err.Error())
+	}
+
 	// Get newly created user.
-	r = Get("/api/user/"+id2.String(), t)
+	r = Get("/api/user/"+user.Id, t)
 	if r != 200 {
-		t.Errorf("GET /api/user/%s did not return 200, instead returned %d\n", id2.String(), r)
+		t.Errorf("GET /api/user/%s did not return 200, instead returned %d\n", user.Id, r)
 	}
 
 	// Update the user's record.
-	r = Put("/api/user/"+id2.String(), "{\"id\": \""+id2.String()+"\", \"username\":\"fdsa\", \"password\": \"fdsa\"}", t)
+	r = Put("/api/user/"+user.Id, "{\"id\": \""+user.Id+"\", \"username\":\"fdsa\", \"password\": \"fdsa\"}", t)
 	if r != 200 {
-		t.Errorf("PUT /api/user/%s did not return 200, instead returned %d\n", id2.String(), r)
+		t.Errorf("PUT /api/user/%s did not return 200, instead returned %d\n", user.Id, r)
 	}
 
 	// DELETE /api/user/{id}
@@ -98,7 +104,7 @@ func testUser(t *testing.T) {
 	}
 
 	// Delete user record.
-	r = Delete("/api/user/"+id2.String(), t)
+	r = Delete("/api/user/"+user.Id, t)
 	if r != 204 {
 		t.Errorf("DELETE did not return 204, instead returned %d\n", r)
 	}
@@ -135,18 +141,23 @@ func Get(path string, t *testing.T) int {
 }
 
 // Make a POST request to the specified path and with the given JSON body.
-func Post(path, body string, t *testing.T) int {
+func Post(path, body string, t *testing.T) ([]byte, int) {
 	post_url := fmt.Sprintf("http://127.0.0.1:8080%s", path)
 	bodyType := "application/json"
 	res, err := http.Post(post_url, bodyType, strings.NewReader(body))
 	if err != nil {
 		t.Errorf(err.Error())
-		return 0
+		return nil, 0
 	}
 	defer res.Body.Close()
+	resbody, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		t.Errorf(err.Error())
+		return nil, 0
+	}
 
 	fmt.Printf("POST %s: %d\n", path, res.StatusCode)
-	return res.StatusCode
+	return resbody, res.StatusCode
 }
 
 // Make a PUT request to the specified path with the given JSON body
