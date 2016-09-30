401c6bd1dc0ff61abcff98188920e6415f820fb6
Brady Sullivan <brady@bsull.com>
2016-09-30 01:04:35 -0700

    Add a test for Sending an unread message.

--- user_test.go
+++ user_test.go
@@ -26,8 +26,24 @@ func TestUserLifecycle(t *testing.T) {
 		t.Errorf("On user delete: %s", err.Error())
 	}
 
-	user, err = db.GetUserByUsername("username")
+	_, err = db.GetUserByUsername("username")
 	if err != ErrUserNotFound {
 		t.Errorf("Database found deleted user")
+	}
+}
+
+func TestMessageSend(t *testing.T) {
+	var err error
+	db, err = BoltDBOpen(DBFILE)
+	if err != nil {
+		t.Errorf("On db open: %s", err.Error())
+		return
+	}
+	defer db.Close()
+
+	u1, _ := NewUser("user1", "password")
+	m1 := NewMessage(MBODY)
+	if err = u1.SendMessage(m1); err != nil {
+		t.Errorf("On message send: %s", err.Error())
 	}
 }
