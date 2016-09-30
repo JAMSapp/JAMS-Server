ebcd9db491555661471f9383296839c915d0829d
Brady Sullivan <brady@bsull.com>
2016-09-29 20:59:48 -0700

    Fix weird merge bug. Tests work again.

--- routes_test.go
+++ routes_test.go
@@ -150,14 +150,8 @@ func Post(path, body string, t *testing.T) ([]byte, int) {
 		return nil, 0
 	}
 	defer res.Body.Close()
-	resbody, err := ioutil.ReadAll(res.Body)
-	if err != nil {
-		t.Errorf(err.Error())
-		return nil, 0
-	}
 
 	resbody, err := ioutil.ReadAll(res.Body)
-
 	if err != nil {
 		t.Errorf(err.Error())
 		return nil, 0
