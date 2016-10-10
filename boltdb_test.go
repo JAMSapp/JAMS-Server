8d68f2a9154fa1c9f170751149b2be98d15cccc1
Brady Sullivan <brady@bsull.com>
2016-10-09 23:38:14 -0700

    Refactor all of the BoltDB tests for users.

--- boltdb_test.go
+++ boltdb_test.go
@@ -7,8 +7,13 @@ import (
 )
 
 var ID = uuid.NewV1().String()
+var ID2 = uuid.NewV1().String()
+
 var USER = "test1"
 var PASS = "hunter2"
+
+var user = &User{Id: ID, Username: USER, Password: PASS}
+var userDupeName = &User{Id: ID2, Username: USER, Password: PASS}
 
 func TestBoltDBOpen(t *testing.T) {
 	db, err := BoltDBOpen(DBFILE)
@@ -22,6 +27,157 @@ func TestBoltDBOpen(t *testing.T) {
 	_, err = BoltDBOpen(DBFILE)
 	if err == nil {
 		t.Errorf("boltdb: error expected with opening dir")
+	}
+}
+
+func TestBoltSaveUser(t *testing.T) {
+	// Setup DB
+	db, err := BoltDBOpen(DBFILE)
+	if err != nil {
+		t.Errorf(err.Error())
+		return
+	}
+	defer db.Conn.Close()
+
+	var nilUser *User
+	nilUser = nil
+	// Test saving a nil user.
+	err = db.SaveUser(nilUser)
+	if err != ErrUserObjectNil {
+		t.Errorf("Saving user with nil object did not return ErrUserObjectNil")
+	}
+
+	// Test regular ol' save.
+	err = db.SaveUser(user)
+	if err != nil {
+		t.Errorf(err.Error())
+	}
+
+	// Test a user with a duplicate username.
+	err = db.SaveUser(userDupeName)
+	if err != ErrUsernameAlreadyExists {
+		t.Errorf("Saving user with duplicate username did not return ErrUsernameAlreadyExists")
+	}
+}
+
+func TestBoltGetUserById(t *testing.T) {
+	// Setup DB
+	db, err := BoltDBOpen(DBFILE)
+	if err != nil {
+		t.Errorf(err.Error())
+		return
+	}
+	defer db.Conn.Close()
+
+	// Test getting user with blank Id
+	u, err := db.GetUserById("")
+	if err != ErrIdCannotBeEmpty {
+		t.Errorf("Getting user with blank id did not return ErrIdCannotBeEmpty")
+	}
+	if u != nil {
+		t.Errorf("Getting user with blank username did not return nil user")
+	}
+
+	// Test getting user with unknown Id
+	u, err = db.GetUserById("1234-1234-1234-1234")
+	if err != ErrUserNotFound {
+		t.Errorf("Getting user with no known Id did not return ErrUserNotFound")
+	}
+	if u != nil {
+		t.Errorf("Getting user with blank username did not return nil user")
+	}
+
+	// Test getting user by a real Id.
+	u, err = db.GetUserById(user.Id)
+	if err != nil {
+		t.Errorf(err.Error())
+	}
+	if u == nil {
+		t.Errorf("GetUserById returned nil user")
+	} else {
+		testUsersEqual(u, user, t)
+	}
+}
+
+func TestBoltGetUserByUsername(t *testing.T) {
+	// Setup DB
+	db, err := BoltDBOpen(DBFILE)
+	if err != nil {
+		t.Errorf(err.Error())
+		return
+	}
+	defer db.Conn.Close()
+
+	// Test getting user with blank username
+	u, err := db.GetUserByUsername("")
+	if err != ErrUsernameCannotBeEmpty {
+		t.Errorf("Getting user with blank username did not return ErrIdCannotBeEmpty")
+	}
+	if u != nil {
+		t.Errorf("Getting user with blank username did not return nil user")
+	}
+
+	// Test getting username by unknown username
+	u, err = db.GetUserByUsername("1234-1234-1234-1234")
+	if err != ErrUserNotFound {
+		t.Errorf("Getting user with no known Username did not return ErrUserNotFound")
+	}
+	if u != nil {
+		t.Errorf("Getting user with unknown username did not return nil user")
+	}
+
+	// Test getting user by a real username
+	u, err = db.GetUserByUsername(user.Username)
+	if err != nil {
+		t.Errorf(err.Error())
+	} else {
+		if u == nil {
+			t.Errorf("GetUserById returned nil user")
+		} else {
+			testUsersEqual(u, user, t)
+		}
+	}
+}
+
+func TestBoltGetAllUsers(t *testing.T) {
+	// Setup DB
+	db, err := BoltDBOpen(DBFILE)
+	if err != nil {
+		t.Errorf(err.Error())
+		return
+	}
+	defer db.Conn.Close()
+
+	users, err := db.GetAllUsers()
+	if err != nil {
+		t.Errorf(err.Error())
+	}
+	if len(users) == 0 {
+		t.Errorf("GetAllUsers had an unexpected length of 0")
+	}
+}
+
+func TestBoltDeleteUser(t *testing.T) {
+	// Setup DB
+	db, err := BoltDBOpen(DBFILE)
+	if err != nil {
+		t.Errorf(err.Error())
+		return
+	}
+	defer db.Conn.Close()
+
+	var nilUser *User
+	nilUser = nil
+	// Test saving a nil user.
+	err = db.DeleteUser(nilUser)
+	if err != ErrUserObjectNil {
+		t.Errorf("Saving user with nil object did not return ErrUserObjectNil")
+	}
+
+	// Delete this test user form the DB.
+	err = db.DeleteUser(user)
+	if err != nil {
+		t.Errorf(err.Error())
 	}
 }
 
@@ -41,93 +197,6 @@ func TestBoltMessageLifecycle(t *testing.T) {
 		t.Errorf(err.Error())
 	}
 
-}
-
-func TestBoltUserLifecycle(t *testing.T) {
-	user := &User{
-		Id:       ID,
-		Username: USER,
-		Password: PASS,
-	}
-
-	db, err := BoltDBOpen(DBFILE)
-	if err != nil {
-		t.Errorf(err.Error())
-		return
-	}
-	defer db.Conn.Close()
-
-	err = db.SaveUser(user)
-	if err != nil {
-		t.Errorf(err.Error())
-	}
-
-	// Make sure GetByUserId returns correct user
-	user2, err := db.GetUserById(user.Id)
-	if err != nil {
-		t.Errorf(err.Error())
-	}
-	testUsersEqual(user, user2, t)
-
-	// Make sure GetByUserByUsername returns correct user
-	user3, err := db.GetUserByUsername(user.Username)
-	if err != nil {
-		t.Errorf(err.Error())
-	}
-	testUsersEqual(user, user3, t)
-
-	_, err = db.GetAllUsers()
-	if err != nil {
-		t.Errorf(err.Error())
-	}
-
-	err = db.DeleteUser(user)
-	if err != nil {
-		t.Errorf(err.Error())
-	}
-
-	// Test getting a user by an ID that doesn't exist.
-	user, err = db.GetUserById(ID)
-	if err != ErrUserNotFound {
-		t.Errorf("Found deleted user")
-	}
-	if user != nil {
-		t.Errorf("GetUserById should have returned nil")
-	}
-
-	// Test saving a nil user.
-	user = nil
-	err = db.SaveUser(user)
-	// Should error out with this specific error.
-	if err != ErrUserObjectNil {
-		if err == nil {
-			t.Errorf("SaveUser should have returned an error.")
-		} else {
-			t.Errorf(err.Error())
-		}
-	}
-
-	// Test failure on GetUserByUsername
-	_, err = db.GetUserByUsername("")
-	if err != ErrUsernameCannotBeEmpty {
-		if err == nil {
-			t.Errorf("GetUserByUsername should have returned an error for blank input.")
-		} else {
-			t.Errorf("Should have gotten ErrUsernameCannotBeEmpty, instead go %s\n", err.Error())
-		}
-	}
-
-	// Test failure on GetUserById.
-	_, err = db.GetUserById("")
-	if err != ErrIdCannotBeEmpty {
-		if err == nil {
-			t.Errorf("GetUserById should have returned an error for blank input.")
-		} else {
-			t.Errorf("Should have gotten ErrIdCannotBeEmpty, instead go %s\n", err.Error())
-		}
-	}
-
-	return
 }
 
 func TestBoltMessageSend(t *testing.T) {
